a335606fdfb04b6792af95ac578de54d004dd5b4
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-07-12 21:29:20 +0530

    Intialised App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { React } from 'react';
 import './App.scss';
+import Inputs from './components/inputs';
+import SaveButton from './components/saveButton';
+import StudentMarkList from './components/studentMarkList';
 
-const App = () =>
-	<div className="App" role="App">
-		Ready to start.
-	</div>;
+const App = (context) => <div className="App" role="App">
+	<h1>Students Database - Add Student Details</h1>
+	<div>{Inputs(context)}</div>
+	<div>{SaveButton(context)}</div>
+	<div className="grid-container" id="grid-container">
+		{ StudentMarkList(context)}
+	</div>
+</div>;
 
 export default App;
